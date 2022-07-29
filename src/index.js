626177dd3353ee9801a95789ea2dc52e55901ef8
Ankit Acharya <acharya.ankit417@gmail.com>
2022-07-29 08:41:31 +0545

    webpack dev server

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import panzoom from "panzoom";
+import flicking from "@egjs/flicking";
+
+console.log(panzoom);
+console.log(flicking);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "Webpack Example App",
+      header: "Webpack Example Title",
+      metaDesc: "Webpack Example Description",
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  mode: "development",
+  output: {
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+};
